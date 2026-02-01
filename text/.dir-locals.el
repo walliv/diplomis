b7b6b6d965a8c2a2907a829913d8db72cce05ba7
Vladislav Valek <vladislav.valek@email.cz>
2026-02-01 11:23:04 +0100

    feat(emacs): set directory locals

---
+++ text/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((LaTeX-mode . ((TeX-master . "../template-thesis")))
+ (TeX-mode . ((TeX-master . "../template-thesis"))))
